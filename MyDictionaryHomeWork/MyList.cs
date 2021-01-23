243b57f4f9b058f90ee5c8f883ccc51a628e9440
Onur Özdemir <ozdemironur1010@gmail.com>
2021-01-23 19:35:35 +0300

    Add project files.

---
+++ MyDictionaryHomeWork/MyList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryHomeWork
+{
+    class MyList<T>
+    {
+        
+        T[] item;
+        T[] tempArray;
+        public MyList()//Constructor
+        {
+            item = new T[0];
+        }
+        public void Add(T items)
+        {
+            tempArray = item;
+            item = new T[item.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                item[i] = tempArray[i];
+            }
+        }
+    }
+}
+
---
+++ MyDictionaryHomeWork/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyDictionaryHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> course1 = new MyList<string>();
+            course1.Add("Java");
+            
+        }
+    }
+}
